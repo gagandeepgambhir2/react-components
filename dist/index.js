78fb84fc8f87bad01e57a7b7b9fcd8040020efd0
gagandeepgambhir2 <gagandeepgambhir2@gmail.com>
2024-12-07 13:20:35 +0530

    rempve extra code and publish project

--- dist/index.js
+++ dist/index.js
@@ -9,14 +9,4 @@ Object.defineProperty(exports, "TextInput", {
     return _TextInput.TextInput;
   }
 });
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-require("./index.css");
-var _App = _interopRequireDefault(require("./App"));
-var _jsxRuntime = require("react/jsx-runtime");
 var _TextInput = require("./components/TextInput");
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-const root = _client.default.createRoot(document.getElementById("root"));
-root.render(/*#__PURE__*/(0, _jsxRuntime.jsx)(_react.default.StrictMode, {
-  children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_App.default, {})
-}));
