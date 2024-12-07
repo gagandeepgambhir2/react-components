31c4c01fdd27bc28259793bdb1362b064652ab83
gagandeepgambhir2 <gagandeepgambhir2@gmail.com>
2024-12-07 14:10:42 +0530

    change version

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true,
+  value: true
 });
 Object.defineProperty(exports, "TextInput", {
   enumerable: true,
   get: function () {
     return _TextInput.TextInput;
-  },
+  }
 });
 var _TextInput = require("./components/TextInput");
